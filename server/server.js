f51c066fe804decc4ceb2c605f95f720f8608c2e
h23linla <h23linla@du.se>
2024-12-17 11:08:19 +0100

    Uppgift 3: Sqlite3, kommunikation med databas

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const server = express();
+const sqlite3 = require("sqlite3").verbose();
+
+server.use(express.json()).use(express.urlencoded({ extended: false }));
 
 server.get("/", (req, res) => {
     const method = req.method;
     const url = req.url;
-    res.send(`Du gjorde en ${method} förfrågan till url:en ${url}`)
+    const db = new sqlite3.Database("./gik339-labb2.db");
+
+    const sql = "SELECT * FROM users";
+    db.all(sql, (err, rows) => {
+        res.send(rows);
+    });
+
+    //res.send(`Du gjorde en ${method} förfrågan till url:en ${url}`)
+});
+
+server.post("/", (req, res) => {
+    const body = req.body;
+    res.status(500).send("test");
 });
 
 server.listen(3000, () =>
-    console.log("Servern körs")
+    console.log("Servern körs.")
 );
 
 server
